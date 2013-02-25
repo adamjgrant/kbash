8e3bff7a4c947d588067790db7e8fa9b06fd2eae
Adam Kochanowicz <ajkochanowicz@gmail.com>
2013-02-25 16:58:20 -0500

    checkin in

--- main.js
+++ main.js
@@ -20,251 +20,254 @@ Array.prototype.remove = function(from, to) {
 // JQUERY DEPENDENT CODE
 // =====================
 
-      (function($)
-      {
-          jQuery.fn.putCursorAtEnd = function()
-          {
-          return this.each(function()
-          {
-              $(this).focus()
-
-              // If this function exists...
-              if (this.setSelectionRange)
-              {
-              // ... then use it
-              // (Doesn't work in IE)
-
-              // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
-              var len = $(this).val().length * 2;
-              this.setSelectionRange(len, len);
-              }
-              else
-              {
-              // ... otherwise replace the contents with itself
-              // (Doesn't work in Google Chrome)
-              $(this).val($(this).val());
-              }
-
-              // Scroll to the bottom, in case we're in a tall textarea
-              // (Necessary for Firefox and Google Chrome)
-              this.scrollTop = 999999;
-          });
-          };
-      })(jQuery);
-
-      // DOM CREATION
-      // ============
-
-      // Quick lookup of flags, args, and opts.
-      Array.prototype.have = function (value) {
-        return this.indexOf(value) > -1 ? true : false; 
+(function($) {
+  jQuery.fn.putCursorAtEnd = function() {
+    return this.each(function() {
+      $(this).focus()
+      // If this function exists...
+      if (this.setSelectionRange) {
+        // ... then use it
+        // (Doesn't work in IE)
+        // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+        var len = $(this).val().length * 2;
+        this.setSelectionRange(len, len);
       }
-
-      $('body').append('<footer class="debug"></footer>');
-      $('footer.debug')
-        .append('<input>')
-        .append('<nav class="console"></nav>')
-        .append('<nav class="input"></nav>')
-      $('nav.input')
-        .append('&gt;&nbsp;<span class="input"></span>')
-        .append('<span class="cursor"></span>')
-      $('span.cursor').append('&#9617;')
-
-      // VARIABLES (JQ DEPENDENT)
-      // ========================
-
-      var nav = $('footer.debug nav')
-      , input = $('footer.debug input')
-      , span = $('span.cursor')
-      , mainConsole = $('nav.console')
-      , fakeInput = $('span.input')
-
-      // NAV -> INPUT CLICK DEFERMENT
-      // ============================
-
-      $(nav).click(function() {
-        $(input).focus()
-        $(span).html('&#9612;')
-        $(mainConsole).fadeIn('fast')
-      });
-      $(input).on('blur', function() {
-        $(span).html('&#9617;')
-        if (!mouseIn) $(mainConsole).fadeOut('fast')
-      })
-
-      // Don't hide the console if we're trying to drag on it.
-      $(mainConsole).hover(
-        function() { mouseIn = true; },
-        function() { mouseIn = false; }
-      )
-
-      // KEY-READING
-      // ===========
-
-      $(input).keyup(function(e) {
-          setCursor(0)
-          // Enter key
-          if( e.keyCode == 13 ) {
-            e.preventDefault();
-            if ($(input).val().length > 0) {
-              setCommandArray( $(input).val() )
-              commandSpot = 0
-            }
-          }
-      });
-      $(input).keydown(function(e) {
-        if (e.keyCode == 38) { // Up arrow
-          // TODO: Set lower limit
-          e.preventDefault();
-          commandSpot--
-          setInput()
-        }
-        if (e.keyCode == 40) { // Down arrow
-          // TODO: Set upper limit
-          e.preventDefault();
-          commandSpot++
-          setInput()
-        }
-        if (e.keyCode == 27) { // Escape
-          e.preventDefault();
-          $('a').focus();
-        }
-      });
-
-      // GETTERS
-      // =======
-
-      function getCommandArray(index) {
-        return commandArray[index] 
+      else {
+        // ... otherwise replace the contents with itself
+        // (Doesn't work in Google Chrome)
+        $(this).val($(this).val());
       }
-      function getResponse(request) {
+      // Scroll to the bottom, in case we're in a tall textarea
+      // (Necessary for Firefox and Google Chrome)
+      this.scrollTop = 999999;
+    });
+  };
+})(jQuery);
 
-        var command = request.split(' ');
+// DOM CREATION
+// ============
 
-        // Determine if this is even valid before we do anything else.
-        if (typeof kbash[(command[0])] == "function") {
+// Quick lookup of flags, args, and opts.
+Array.prototype.have = function (value) {
+  return this.indexOf(value) > -1 ? true : false; 
+}
 
-          // Strip args and flags from main command as "params"
-          var params = command.slice(1,command.length)
+$('body').append('<footer class="debug"></footer>');
+$('footer.debug')
+  .append('<input>')
+  .append('<nav class="console"></nav>')
+  .append('<nav class="input"></nav>')
+$('nav.input')
+  .append('&gt;&nbsp;<span class="input"></span>')
+  .append('<span class="cursor"></span>')
+$('span.cursor').append('&#9617;')
 
-          // Create command tree.
-          var commandTree = { 
-            rootCommand: command[0]
-            , args: []
-            , flags: []
-            , opts: []
-            , props: { 
-              empty: true 
-            }
-          }
+// VARIABLES (JQ DEPENDENT)
+// ========================
 
-          for ( var i = 0; i < params.length; i++ ) {
-            if (params[i].substr(0,1) == "-") {
-              // Remove opts ("--option") from params
-              if (params[i].substr(0,2) == "--") { commandTree.opts.push(params[i].substring(2,params[i].length)) }
-              // Remove flags ("-f") from params
-              else { commandTree.flags = commandTree.flags.concat((params[i].substring(1,params[i].length)).split('')) }
-              params.remove(i)
-              i--
-            }
-          }
+var nav = $('footer.debug nav')
+, input = $('footer.debug input')
+, span = $('span.cursor')
+, mainConsole = $('nav.console')
+, fakeInput = $('span.input')
 
-          // We assume everything left are arguments.
-          commandTree.args = params;
+// NAV -> INPUT CLICK DEFERMENT
+// ============================
 
-          // Set some properties for quick access.
-          if ( 
-            commandTree.args.length > 0
-            || commandTree.flags.length > 0
-            || commandTree.opts.length > 0
-          ) {
-            commandTree.props.empty = false;
-          }
+$(nav).click(function() {
+  $(input).focus()
+  $(span).html('&#9612;')
+  $(mainConsole).fadeIn('fast')
+});
+$(input).on('blur', function() {
+  $(span).html('&#9617;')
+  if (!mouseIn) $(mainConsole).fadeOut('fast')
+})
 
-          // Ready to send the flags and args to the root command
-          kbash[commandTree.rootCommand](
-            commandTree.args
-            , commandTree.flags
-            , commandTree.opts
-            , commandTree.props
-          ) 
-        }
+// Don't hide the console if we're trying to drag on it.
+$(mainConsole).hover(
+  function() { mouseIn = true; },
+  function() { mouseIn = false; }
+)
 
-        // Invalid command
-        else {
-          console.kbash('-kbash: ' + request + ': command not found.') 
-        }
+// KEY-READING
+// ===========
+
+$(input).keyup(function(e) {
+    setCursor(0)
+    // Enter key
+    if( e.keyCode == 13 ) {
+      e.preventDefault();
+      if ($(input).val().length > 0) {
+        setCommandArray( $(input).val() )
+        commandSpot = 0
       }
+    }
+});
+$(input).keydown(function(e) {
+  if (e.keyCode == 38) { // Up arrow
+    // TODO: Set lower limit
+    e.preventDefault();
+    commandSpot--
+    setInput()
+  }
+  if (e.keyCode == 40) { // Down arrow
+    // TODO: Set upper limit
+    e.preventDefault();
+    commandSpot++
+    setInput()
+  }
+  if (e.keyCode == 27) { // Escape
+    e.preventDefault();
+    $('a').focus();
+  }
+});
 
-      // SETTERS
-      // =======
+// GETTERS
+// =======
 
-      function setInput() {
-        var newString = getCommandArray((commandArray.length + commandSpot))
-        $(input).val(newString)
-        setCursor()
-        $('body').focus();
-        $(input).putCursorAtEnd();
-      }
-      function setCommandArray(item) {
-        commandArray = commandArray.concat(item)
-        console.kbash('> ' + item)
-        setRequest(item)
-        $(input).val('')
-        setCursor()
-        localStorage.setItem('commandArray', JSON.stringify(commandArray))
-      }
-      console.kbash = function (string) {
-         $(mainConsole).append('<p>' + string +'</p>').scrollTop($(mainConsole)[0].scrollHeight)
-      }
-      function setCursor(spot) {
-        // Write in the nav
-        spot = (spot || cursorSpot)  
-        $(fakeInput).html($(input).val());
-        // TODO: Allow the user to control cursor with arrow keys.
-      }
-      function setRequest(request) {
-         // TODO: Parse for flags, commands, arguments and options.
-         getResponse(request);
-      }
-      
-      // COLORATION
+function getCommandArray(index) {
+  return commandArray[index] 
+}
+function getResponse(request) {
 
-      kbash.e = function(string) { return '<span class="red">' + string + '</span>' }
-      kbash.s = function(string) { return '<span class="green">' + string + '</span>' }
+  var command = request.split(' ');
 
-      // USER HELP
+  // Determine if this is even valid before we do anything else.
+  if (typeof kbash[(command[0])] == "function") {
 
-      kbash.help = function(args, flags, opts, props) {
-        var objs = Object.getOwnPropertyNames(kbash)
-        console.kbash('<span class="white">AVAILABLE COMMANDS</span>')
-        console.kbash('<span class="white">==================</span>')
-        for (var i = 0; i < objs.length; i++ ) {
-          console.kbash(objs[i])
-        }
-        console.kbash('&nbsp;')
-        console.kbash('<span class="white">DEMO</span>')
-        console.kbash('<span class="white">====</span>')
-        msg = flags.have('v') ? 'try "say hello [-v] [--polite] [--rude]"' : 'Try "say hello" (or "help -v" for more options)'
-        console.kbash(msg)
+    // Strip args and flags from main command as "params"
+    var params = command.slice(1,command.length)
+
+    // Create command tree.
+    var commandTree = { 
+      rootCommand: command[0]
+      , args: []
+      , flags: []
+      , opts: []
+      , props: { 
+        empty: true 
       }
-      kbash.math = function(args, flags, opts, props) {
-        for ( var i =0; i < args.length; i++ ) {
-          try {
-            if (args[i].match(/^[-*/+0-9]+$/)) { 
-              console.kbash(args[i] + ' = <span class="white">' + parseFloat(eval(args[i])) + '</span>')
-            }
-            else { 
-              console.kbash(kbash.e('ERROR') + ' Insecure string detected') 
-              console.kbash('To prevent a script attack, this input will not be calculated.')
-            }
-          }
-          catch(e) {
-            console.kbash(kbash.e('ERROR') + ' Could not compute.')
-            console.kbash('(' + e + ')');
-          }
-        }
+    }
+
+    for ( var i = 0; i < params.length; i++ ) {
+      if (params[i].substr(0,1) == "-") {
+        // Remove opts ("--option") from params
+        if (params[i].substr(0,2) == "--") { commandTree.opts.push(params[i].substring(2,params[i].length)) }
+        // Remove flags ("-f") from params
+        else { commandTree.flags = commandTree.flags.concat((params[i].substring(1,params[i].length)).split('')) }
+        params.remove(i)
+        i--
       }
+    }
+
+    // We assume everything left are arguments.
+    commandTree.args = params;
+
+    // Set some properties for quick access.
+    if ( 
+      commandTree.args.length > 0
+      || commandTree.flags.length > 0
+      || commandTree.opts.length > 0
+    ) {
+      commandTree.props.empty = false;
+    }
+
+    // Ready to send the flags and args to the root command
+    kbash[commandTree.rootCommand](
+      commandTree.args
+      , commandTree.flags
+      , commandTree.opts
+      , commandTree.props
+    ) 
+  }
+
+  // Invalid command
+  else {
+    console.kbash('-kbash: ' + request + ': command not found.') 
+  }
+}
+
+// SETTERS
+// =======
+
+function setInput() {
+  var newString = getCommandArray((commandArray.length + commandSpot))
+  $(input).val(newString)
+  setCursor()
+  $('body').focus();
+  $(input).putCursorAtEnd();
+}
+function setCommandArray(item) {
+  commandArray = commandArray.concat(item)
+  console.kbash('> ' + item)
+  setRequest(item)
+  $(input).val('')
+  setCursor()
+  localStorage.setItem('commandArray', JSON.stringify(commandArray))
+}
+console.kbash = function (string) {
+   $(mainConsole).append('<p>' + string +'</p>').scrollTop($(mainConsole)[0].scrollHeight)
+}
+function setCursor(spot) {
+  // Write in the nav
+  spot = (spot || cursorSpot)  
+  $(fakeInput).html($(input).val());
+  // TODO: Allow the user to control cursor with arrow keys.
+}
+function setRequest(request) {
+   // TODO: Parse for flags, commands, arguments and options.
+   getResponse(request);
+}
+
+// COLORATION
+
+kbash.e = function(string) {
+  var string = (string || 'ERROR:')
+  return '<span class="red">' + string + '</span>' 
+}
+kbash.s = function(string) {
+  var string = (string || 'SUCCESS:')
+  return '<span class="green">' + string + '</span>' 
+}
+
+// USER HELP
+
+kbash.help = function(args, flags, opts, props) {
+  var objs = Object.getOwnPropertyNames(kbash)
+  console.kbash('<span class="white">AVAILABLE COMMANDS</span>')
+  console.kbash('<span class="white">==================</span>')
+  for (var i = 0; i < objs.length; i++ ) {
+    console.kbash(objs[i])
+  }
+  console.kbash('&nbsp;')
+  console.kbash('<span class="white">DEMO</span>')
+  console.kbash('<span class="white">====</span>')
+  msg = flags.have('v') ? 'try "say hello [-v] [--polite] [--rude]"' : 'Try "say hello" (or "help -v" for more options)'
+  console.kbash(msg)
+}
+kbash.math = function(args, flags, opts, props) {
+  if (props.empty) { console.kbash('Please provide an equation to calculate') }
+  for ( var i =0; i < args.length; i++ ) {
+    try {
+      if (args[i].match(/^[-*/+0-9]+$/)) { 
+        console.kbash(args[i] + ' = <span class="white">' + parseFloat(eval(args[i])) + '</span>')
+      }
+      else { 
+        console.kbash(kbash.e() + ' Insecure string detected') 
+        console.kbash('To prevent a script attack, this input will not be calculated.')
+      }
+    }
+    catch(e) {
+      console.kbash(kbash.e() + ' Could not compute.')
+      console.kbash('(' + e + ')');
+    }
+  }
+}
+kbash.edit = function() {
+  document.body.contentEditable='true'; document.designMode='on'; void 0;
+  console.kbash(kbash.s('EDITING MODE') + ': You may now edit this website.')
+}
 
 /*
 When people see some things as beautiful,
